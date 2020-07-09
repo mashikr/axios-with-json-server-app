8ec585738bb82f4c8fb284dd70828d093dce6f1a
ashik <mdashikur567@gmail.com>
2020-07-09 13:17:16 +0600

    add send data process

--- js/index.js
+++ js/index.js
@@ -2,16 +2,19 @@ let url = "http://localhost:3000/students/";
 
 // Get data onload window
 window.onload = function(){
-    let dataBody = document.getElementById("databody");
+    let dataBody = document.getElementById("dataBody");
     axios.get(url)
     .then(res => {
         res.data.forEach(data => {
-            console.log(data);
-            console.log(dataBody);
             dataElement(data, dataBody);
         });
     })
     .catch(err => console.log(err));
+
+    let btnAdd = document.getElementById('btnAdd');
+    btnAdd.addEventListener('click',function(){
+        sendData();
+    });
 }
 
 // data element
@@ -27,11 +30,46 @@ function dataElement(data, parentElement) {
             <b>Email:</b> ${data.email}
           </div>
           <div class="card-footer">
-            <button class="btn btn-warning">Edit</button>
+            <button class="btn btn-warning" id="${data.id}">Edit</button>
             <button class="btn btn-danger float-right">Delete</button>
           </div>
         </div>
       </div>
     `;
     parentElement.insertAdjacentHTML('beforeend', markup);
+    document.getElementById(data.id).addEventListener('click', () => {
+        console.log(data.id);
+        console.log(data.name);
+        console.log(data.dept);
+        console.log(data.roll);
+    })
+}
+
+// send data
+function sendData(){
+    let nameField = document.getElementById('nameField');
+    let deptField = document.getElementById('deptField');
+    let rollField = document.getElementById('rollField');
+    let phoneField = document.getElementById('phoneField');
+    let emailField = document.getElementById('emailField');
+
+    let data = {
+        name: nameField.value,
+        dept: deptField.value,
+        roll: rollField.value,
+        phone: phoneField.value,
+        email: emailField.value
+    }
+
+    axios.post(url, data)
+        .then(res => {
+            let dataBody = document.getElementById("dataBody");
+            dataElement(res.data, dataBody);
+            nameField.value = '';
+            deptField.value = '';
+            rollField.value = '';
+            phoneField.value = '';
+            emailField.value = '';
+        })
+        .catch(err => console.log(err))
 }
